1981a9013348b9f74bd26db05c334b32c1ea4807
lcsmedeiros <lucas.a.medeiros1@gmail.com>
2020-09-04 17:20:55 -0300

    add querySelector

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 window.onload = remover;
 
 function remover() {
-    conteudo = document.querySelector('.css-1dbjc4n.r-1ysxnx4.r-k0dy70.r-1ylenci.r-1phboty.r-rs99b7.r-dr54s0');
+    conteudo = document.querySelector('[data-testid=sidebarColumn]')
     conteudo.parentNode.removeChild(conteudo)
 }
