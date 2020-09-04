dae7304b8b1e94d9a3008d7859ee65b7605d2652
lcsmedeiros <lucas.a.medeiros1@gmail.com>
2020-09-04 16:47:39 -0300

    first commit

---
+++ content.js
@@ -0,0 +1,6 @@
+window.onload = remover;
+
+function remover() {
+    conteudo = document.querySelector('.css-1dbjc4n.r-1ysxnx4.r-k0dy70.r-1ylenci.r-1phboty.r-rs99b7.r-dr54s0');
+    conteudo.parentNode.removeChild(conteudo)
+}
